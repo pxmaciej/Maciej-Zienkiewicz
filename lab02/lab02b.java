ecc3acae544cf633a47cbdce3e5044f785b7c7be
Maciej Zienkiewicz <averestick@gmail.com>
2019-10-16 20:28:19 +0200

    Koniec

--- lab02/lab02b.java
+++ lab02/lab02b.java
@@ -15,7 +15,7 @@ class Student {
 	public String studentNames;
 	public String studentSurnames;
 	public boolean studentExist;
-	public String om;
+	public String omo4;
 
 	public void setStudentNo(String studentNo) {
 		this.studentNo = studentNo;
