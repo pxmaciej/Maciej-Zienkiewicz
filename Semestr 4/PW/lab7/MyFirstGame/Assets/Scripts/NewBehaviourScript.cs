175447449a67b16a92004230697563542d61d983
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-05-10 23:27:50 +0200

    Lab7

---
+++ Semestr 4/PW/lab7/MyFirstGame/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int ilosc = 10;
+    string myname = "Maciej Zienkiewicz";
+    void Start()
+    {
+        for (int i = 0; i < ilosc; i++)
+        {
+            print(myname);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
