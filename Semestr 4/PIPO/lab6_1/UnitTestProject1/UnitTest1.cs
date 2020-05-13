3405f4f3e504caf5fe8ea56d714dec95bd71e4b9
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-05-13 20:03:59 +0200

    Lab6_1 Unit tests

---
+++ Semestr 4/PIPO/lab6_1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6_1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void silnia()
+        {
+            Silnia sil = new Silnia();
+            sil.n = 0;
+            Assert.AreEqual(sil.oblicz(), 1);
+            sil.n = 1;
+            Assert.AreEqual(sil.oblicz(), 1);
+            sil.n = 5;
+            Assert.AreEqual(sil.oblicz(), 120);
+
+
+        }
+    }
+}
---
+++ Semestr 4/PIPO/lab6_1/lab6_1/silnia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+   public class Silnia
+    {
+        public int n;
+
+        public int oblicz()
+        {
+           int temp = 1;
+            for (int i = n; i > 1; i--)
+            {
+                temp *= i; 
+            }
+               
+
+            return temp;
+        }
+    }
+}
