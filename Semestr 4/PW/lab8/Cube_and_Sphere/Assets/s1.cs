217bfcdd5192b93a6080c5182e3ee0cc07191db6
Maciej Zienkiewicz <pxmaciej@outlook.com>
2020-05-15 23:17:38 +0200

    lab8

---
+++ Semestr 4/PW/lab8/Cube_and_Sphere/Assets/s1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+   
+    GameObject[] find;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        find = GameObject.FindGameObjectsWithTag("kula");
+        foreach(GameObject temp in find)
+        {
+            Destroy(temp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
